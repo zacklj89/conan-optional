9191e0ebf6bc6001e580e8b802b1093b7cd100c3
Zack Johnson <zachary.johnson@keysight.com>
2017-10-02 14:44:36 -0400

    updating cmakelists

--- conanfile.py
+++ conanfile.py
@@ -7,8 +7,10 @@ class OptionalConan(ConanFile):
     version = '2.1.0'
     description = 'std::optional lightweight implementation'
     url = 'https://github.com/zacklj89/conan-optional'
-    license = 'https://github.com/nlohmann/json/blob/v2.1.0/LICENSE.MIT'
-    settings = None
+    license = 'https://github.com/martinmoene/optional-lite/blob/master/LICENSE'
+    settings = 'os', 'compiler', 'arch', 'build_type'
+    exports_sources = 'CMakeLists.txt'
+    generators = 'cmake'
 
     @property
     def _archive_dirname(self):
@@ -25,11 +27,11 @@ class OptionalConan(ConanFile):
 ## replace url and rename
     def source(self):
         download_url = 'https://github.com/martinmoene/optional-lite/archive/v{!s}.zip'.format(self.version)
-        download(download_url, 'optional.zip')
-        check_sha256('optional.zip','d2b6f6f5e2a7621119bdad963ee1392b22b85a138cced9505f4c006b13820d686')
-        unzip('optional.zip')
-        os.unlink('optional.zip')
-        os.rename(self._archive_dirname, 'optional')
+        download(download_url, 'optional-lite-{!s}.zip'.format(self.version))
+        check_sha256('optional-lite-{!s}.zip'.format(self.version),'d2b6f6f5e2a762119bdad963ee1392b22b85a138cced9505f4c006b13820d686')
+        unzip('optional-lite-{!s}.zip'.format(self.version))
+        os.unlink('optional-lite-{!s}.zip'.format(self.version))
+        os.rename('optional-lite-{!s}'.format(self.version), 'optional')
 
     def build(self):
         cmake = self._run_cmake()    # rerun cmake
